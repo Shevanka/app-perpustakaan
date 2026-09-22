be755873b7070ebb7a98d1605a3be4e6439bfacc
Shevanka Kurniawan <shevanka.kurniawan@gmail.com>
2026-09-22 21:29:50 +0700

    Pengerjaan tugas 2: route group admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ Route::resource('members', MemberController::class);
 Route::resource('loans', LoanController::class);
 Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
     ->name('loans.kembalikan');
+
+// Route Grouping dengan Prefix 'admin' (Tugas Pertemuan 2)
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Admin Info: Sistem Perpustakaan Digital Kampus';
+    });
+});
